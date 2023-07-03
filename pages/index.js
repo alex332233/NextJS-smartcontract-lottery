45a288e27197bdc9dd474823f773448b9d9ad865
Alex Su <alex332233@gmail.com>
2023-07-03 13:33:39 +0800

    fixed typo

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home() {
             <Header />
             {/* <ManualHeader /> */}
             <LotteryEntrance></LotteryEntrance>
-            Hello
         </>
     )
 }
